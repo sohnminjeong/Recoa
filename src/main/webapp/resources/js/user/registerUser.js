b7f6ccf612d3539c1d09e497da10799e5e1d57e1
sohnminjeong <152463277+sohnminjeong@users.noreply.github.com>
2024-08-03 01:51:09 +0900

    <feat> 회원가입 정규표현식 및 중복 확인 완료

--- src/main/webapp/resources/js/user/registerUser.js
+++ src/main/webapp/resources/js/user/registerUser.js
@@ -3,6 +3,9 @@ let idDupCheck = false;
 let pwdCheck = false;
 let userNicknameCheck = false;
 let userPhoneCheck = false;
+let userPhoneDupCheck=false;
+let emailDupCheck = false;
+let emailCheck = false;
 
 // 아이디 정규표현식
 $('#userId').keyup((e) =>{
@@ -71,15 +74,15 @@ $('#userNickname').keyup(() => {
 })
 
 // 핸드폰번호 정규표현식
-$('#userId').keyup((e) =>{
-	let userId=$(e.target).val(); 
-	const regExp = /^[A-Za-z]+[A-Za-z0-9]{3,11}$/;
-	if(regExp.test(userId)){
-		$('#idCheck').text("").css("color", "gray");
-		idCheck = false;
+$('#userPhone').keyup((e) =>{
+	let userPhone=$(e.target).val(); 
+	const regExp = /^01([0,1])-?([0-9]{3,4})-?([0-9]{4})$/;
+	if(regExp.test(userPhone)){
+		$('#userPhoneCheck').text("").css("color", "gray");
+		userPhoneCheck = false;
 	} else {
-		$('#idCheck').text("영문자, 숫자 포함하여 총 4~12자로 입력하세요.").css("color", "gray");
-		idCheck = true;
+		$('#userPhoneCheck').text("-포함하여 작성해주세요").css("color", "gray");
+		userPhoneCheck = true;
 	}
 });
 
@@ -94,11 +97,45 @@ $('#userPhone').keyup(() => {
 		success: function (result) {
 			if (result) {
 				$('#userPhoneCheck').text("중복된 핸드폰번호입니다").css("color", "gray");
-				idDupCheck = true;
-			} else if(!result&&!idCheck) {
+				userPhoneDupCheck = true;
+			} else if(!result&&!userPhoneCheck) {
 				$('#userPhoneCheck').text("").css("color", "gray");
-				idDupCheck = false;
+				userPhoneDupCheck = false;
 			}
 		}
 	})
 })
+
+// 이메일 정규표현식
+$('#userEmail').keyup((e) =>{
+	let userEmail=$(e.target).val(); 
+	const regExp = /^\w+@\w+\.\w+$/;
+	if(regExp.test(userEmail)){
+		$('#emailCheck').text("").css("color", "gray");
+		emailCheck = false;
+	} else {
+		$('#emailCheck').text("이메일 형식에 맞춰서 입력하세요.").css("color", "gray");
+		emailCheck = true;
+	}
+});
+
+// 이메일 중복확인
+$('#userEmail').keyup(() => {
+	const userEmail = $('#userEmail').val();
+	$.ajax({
+		type: "post",
+		url: "/emailCheck",
+		data: "userEmail=" + userEmail,
+
+		success: function (result) {
+			if (result) {
+				$('#emailCheck').text("중복된 이메일입니다").css("color", "gray");
+				emailDupCheck = true;
+			} else if(!result&&!emailCheck) {
+				$('#emailCheck').text("").css("color", "gray");
+				emailDupCheck = false;
+			}
+		}
+	})
+})
+
